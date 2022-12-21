aef31eeb8867cdcdbea1c84d2c0db31a286bbb83
mGlok <gloczek4lol@gmail.com>
2022-12-21 15:29:10 +0100

    Create api methods functionality in api.js file

---
+++ api/api.js
@@ -0,0 +1,88 @@
+const apiUrl = "http://localhost:3000/todos";
+export const getAllToDos = async () => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+    });
+    if (response.status === 200) {
+      return response.json();
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+
+export const createToDo = async (todo) => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ todo }),
+    });
+
+    if (response.status === 200) {
+      return response.json();
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+
+export const editTodo = async (id, todo) => {
+  try {
+    const response = await fetch(`${apiUrl}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id, todo }),
+    });
+    if (response.status === 200) {
+      return response.json();
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+export const setFinished = async (id) => {
+  try {
+    const response = await fetch(`${apiUrl}/setFinished/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    });
+    if (response.status === 200) {
+      return response.json();
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+
+export const setUnfinished = async (id) => {
+  try {
+    const response = await fetch(`${apiUrl}/setUnfinished/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    });
+    if (response.status === 200) {
+      return response.json();
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+
+export const deleteToDo = async (id) => {
+  try {
+    const response = await fetch(`${apiUrl}/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    });
+    if (response.status === 200) {
+      return response.json();
+    }
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
